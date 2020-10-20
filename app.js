c2ea2fa817f227cb5b93ecc2b64a2f4546aa0a22
crownbackend <crownbackend@gmail.com>
2020-10-20 23:24:53 +0200

    user auth first

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser")
 const mongoose = require('mongoose')
 
 const stuffRoutes = require("./routes/stuff")
+const userRoutes = require("./routes/user")
 
 mongoose.connect('mongodb://admin:admin@127.0.0.1:27017/',
     { useNewUrlParser: true,
@@ -22,5 +23,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 
 app.use('/api/stuff', stuffRoutes)
+app.use('/api/auth', userRoutes)
 
 module.exports = app
