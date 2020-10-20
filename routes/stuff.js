017ee04b2e07975602458077245c0e499d9631ff
crownbackend <crownbackend@gmail.com>
2020-10-21 01:00:24 +0200

    upload file post

--- routes/stuff.js
+++ routes/stuff.js
@@ -3,8 +3,9 @@ const router = express.Router()
 
 const stuffController = require('../controllers/stuff')
 const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
 
-router.post("/", auth, stuffController.createThing)
+router.post("/", auth, multer, stuffController.createThing)
 router.put("/:id", auth, stuffController.modifyThing)
 router.delete('/:id', auth, stuffController.deleteThing)
 router.get('/:id', auth, stuffController.getOneThing)
